632c6a1a94460cff17aea704b4409169f8e3d54e
Jay Moker <AC@B>
2025-12-19 11:30:26 +0100

    Added test script

---
+++ test_client_mcp.py
@@ -0,0 +1,41 @@
+import asyncio
+from fastmcp import Client
+
+
+SERVER_URL = "http://localhost:9001/mcp"
+
+client = Client(SERVER_URL)
+
+
+async def main():
+    try:
+        async with client:
+            print(f"Starrrting client ping")
+            await client.ping()
+
+            tools = await client.list_tools()
+            print(f"Available tools: {tools}")
+
+            print(f"Calling tool")
+            result = await client.call_tool(
+                "query_db", {"question": "Resume moi les 2 derniers episodes"}
+            )
+
+            print(f"response: {result}")
+
+        # from src.query import QueryEngine, QueryConfig
+        # config = QueryConfig()
+        # engine = QueryEngine(config)
+        # # response = await engine.query_engine.aquery("Resume moi les 2 derniers episodes")
+        # response = engine.query_engine.query("Resume moi les 2 derniers episodes")
+
+        # print(f"response: {response}")
+
+
+        print("Success")
+    except Exception as e:
+        print(f"Fatal error : {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
