0aee33c10dacb56037f42a7c018441108eaf7b7f
Jay Moker <AC@B>
2025-12-24 10:40:18 +0100

    Added fallback for optionnal param if episode_id not provided.

--- src/storage/local.py
+++ src/storage/local.py
@@ -47,6 +47,8 @@ class LocalStorage(BaseStorage):
         Returns:
             str: The prefix path for the episode workspace.
         """
+        if episode_id is None:
+            episode_id = 0
         workspace_path = f"data/transcripts/episode_{episode_id:03d}/"
         try:
             os.makedirs(workspace_path, exist_ok=True)
