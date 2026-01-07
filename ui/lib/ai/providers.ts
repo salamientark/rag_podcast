d28257dd99598cb853f44b65cc06360f782049a5
Valentin Manes <valentinmanes@outlook.fr>
2026-01-07 03:49:41 +0100

    fix: change front models

--- ui/lib/ai/providers.ts
+++ ui/lib/ai/providers.ts
@@ -5,16 +5,16 @@ import { anthropic } from '@ai-sdk/anthropic';
 
 export const myProvider = customProvider({
   languageModels: {
-    // 'chat-model': openai('gpt-4o'),
-    'chat-model': anthropic('claude-4-sonnet-20250514'),
+    'chat-model': openai('gpt-5.2'),
+    // 'chat-model': anthropic('claude-4-sonnet-20250514'),
     // 'chat-model-reasoning': anthropic('claude-opus-4-20250514'),
     'chat-model-reasoning': openai('gpt-5.2'),
     // 'chat-model-reasoning': google('gemini-2.5-pro'),
-    'title-model': anthropic('claude-4-sonnet-20250514'),
-    'artifact-model': anthropic('claude-3-5-haiku-latest'),
-    gpt: openai('gpt-4o'),
-    // 'title-model': openai('gpt-4o'),
-    // 'artifact-model': openai('gpt-3.5-turbo'),
+    // 'title-model': anthropic('claude-4-sonnet-20250514'),
+    // 'artifact-model': anthropic('claude-3-5-haiku-latest'),
+    // gpt: openai('gpt-4o'),
+    'title-model': openai('gpt-5-nano'),
+    'artifact-model': openai('gpt-3.5-turbo'),
   },
   imageModels: {
     'small-model': openai.image('dall-e-3'),
