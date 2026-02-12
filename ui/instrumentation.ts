55ca3bab3da4ab239dac9e47859e8cdc80820dc1
Jay Moker <AC@B>
2026-02-12 10:19:18 +0100

    - Added Langfuse init in Typescript code

--- ui/instrumentation.ts
+++ ui/instrumentation.ts
@@ -1,7 +1,27 @@
-import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
-
-const tracerProvider = new NodeTracerProvider();
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { LangfuseSpanProcessor } from '@langfuse/otel';
 
 export function register() {
-  tracerProvider.register();
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+	// Register OpenTelemetry for Langfuse tracing
+    const sdk = new NodeSDK({
+      spanProcessors: [new LangfuseSpanProcessor()],
+    });
+
+    sdk.start();
+
+    process.on('SIGTERM', () => {
+      sdk.shutdown()
+        .then(() => console.log('Tracing terminated'))
+        .catch((error) => console.log('Error terminating tracing', error))
+        .finally(() => process.exit(0));
+    });
+
+    process.on('SIGINT', () => {
+      sdk.shutdown()
+        .then(() => console.log('Tracing terminated'))
+        .catch((error) => console.log('Error terminating tracing', error))
+        .finally(() => process.exit(0));
+    });
+  }
 }
